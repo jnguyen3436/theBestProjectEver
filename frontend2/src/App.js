672021c7a3cbec2eb65c328b34d03ca590e8fb79
jnguyen3436 <jnguyen3436@gmail.com>
2019-12-09 10:25:24 -0500

    trying to fix this

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// import logo from './logo.svg';
+import './App.css';
+import { Route,Switch } from "react-router-dom"
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import {Nav} from 'react-bootstrap'
+import "./images/logo-ironhack.png"
+import About from "./Components/About"
+import Home from "./Components/Home"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+         <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="home">
+      <img
+        alt=""
+        src="./images/logo-ironhack.png"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+      The Forge
+    </Navbar.Brand>
+    <Nav className="mr-auto">
+      
+      <Nav.Link href="about">About</Nav.Link>
+      {/* <Nav.Link href="friend">Create A Friend</Nav.Link>
+      <Nav.Link href="generator">Friend Generator</Nav.Link> */}
+    </Nav>
+  </Navbar>
+        {/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Spinny Spin
       </header>
-    </div>
-  );
+    </div> */}
+    <Switch>
+          <Route path= "/about" component={About}/>
+          <Route path= "/home" component={Home}/>
+         
+          </Switch>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
